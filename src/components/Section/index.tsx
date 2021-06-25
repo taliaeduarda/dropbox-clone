6856aa4b3fc7fc3035333d598c9625eef903c1dc
taliaeduarda <taliaeduardaa@gmail.com>
2021-06-24 22:20:11 -0300

    primeiro commit

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    variant: 'blue' | 'beige' | 'white' | 'black';
+    title: string;
+    description: string;
+}
+
+const Section: React.FC<Props> = () => {
+    return <div />
+}
+
+export default Section;
